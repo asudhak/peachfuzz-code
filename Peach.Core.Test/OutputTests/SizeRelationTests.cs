5bdb1642d3ac4cc5e1da44ba9f7a28e95f18b585
meddingt <meddingt@9c48eac0-7620-0410-89eb-927fe2045f54>
2011-06-26 22:37:47 +0000

    Started adding output tests (currently failing)

---
+++ Peach.Core.Test/OutputTests/SizeRelationTests.cs
@@ -0,0 +1,79 @@
+﻿
+//
+// Copyright (c) Michael Eddington
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights 
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+// copies of the Software, and to permit persons to whom the Software is 
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in	
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+// Authors:
+//   Michael Eddington (mike@phed.org)
+
+// $Id$
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using Peach.Core;
+using Peach.Core.Dom;
+using Peach.Core.Analyzers;
+using Peach.Core.Cracker;
+using Peach.Core.IO;
+
+namespace Peach.Core.Test.OutputTests
+{
+	[TestFixture]
+	class SizeRelationTests
+	{
+		[Test]
+		public void OutputSizeOf1()
+		{
+			string xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n<Peach>\n" +
+				"	<DataModel name=\"TheDataModel\">" +
+				"		<Number size=\"8\">" +
+				"			<Relation type=\"size\" of=\"Data\" />" +
+				"		</Number>" +
+				"		<Blob name=\"Data\" />" +
+				"	</DataModel>" +
+				"</Peach>";
+
+			PitParser parser = new PitParser();
+			Dom.Dom dom = parser.asParser(new Dictionary<string, string>(), new MemoryStream(ASCIIEncoding.ASCII.GetBytes(xml)));
+
+			BitStream data = new BitStream();
+			data.WriteInt8((sbyte)"Hello World".Length);
+			data.WriteBytes(ASCIIEncoding.ASCII.GetBytes("Hello World"));
+			data.SeekBits(0, SeekOrigin.Begin);
+
+			DataCracker cracker = new DataCracker();
+			cracker.CrackData(dom.dataModels[0], data);
+
+			byte[] newData = ASCIIEncoding.ASCII.GetBytes("This is a much longer value than before!");
+			dom.dataModels[0][1].DefaultValue = new Variant(newData);
+
+			Assert.AreEqual(newData.Length, (int)dom.dataModels[0][0].InternalValue);
+		}
+	}
+}
+
+// end
+
