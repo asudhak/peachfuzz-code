95951a6f5d31f09bfca0d46d011f462b2d3c6ff4
Thomas Nagy <tnagy2pow10@gmail.com>
2012-02-20 02:06:16 +0100

    fixed the import in the code snippet for profiling

--- waflib/Scripting.py
+++ waflib/Scripting.py
@@ -130,7 +130,7 @@ def waf_entry_point(current_directory, version, wafdir):
 
 	"""
 	import cProfile, pstats
-	cProfile.runctx("import Scripting; Scripting.run_commands()", {}, {}, 'profi.txt')
+	cProfile.runctx("from waflib import Scripting; Scripting.run_commands()", {}, {}, 'profi.txt')
 	p = pstats.Stats('profi.txt')
 	p.sort_stats('time').print_stats(25)
 	"""
