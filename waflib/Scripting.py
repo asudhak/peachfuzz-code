3a1a855ab591a22800a1f3405616693b30402272
Thomas Nagy <tnagy2pow10@gmail.com>
2012-04-09 23:49:07 +0200

    another useful profiling value is 'cumulative'

--- waflib/Scripting.py
+++ waflib/Scripting.py
@@ -132,7 +132,7 @@ def waf_entry_point(current_directory, version, wafdir):
 	import cProfile, pstats
 	cProfile.runctx("from waflib import Scripting; Scripting.run_commands()", {}, {}, 'profi.txt')
 	p = pstats.Stats('profi.txt')
-	p.sort_stats('time').print_stats(25)
+	p.sort_stats('time').print_stats(25) # or 'cumulative'
 	"""
 	try:
 		run_commands()
