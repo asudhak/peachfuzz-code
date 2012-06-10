e91c4a875fe88b9f36c915546955f75c29c80026
sirusdv <sirusdv@9c48eac0-7620-0410-89eb-927fe2045f54>
2012-06-10 02:09:19 +0000

    * ExpressionFixup * ExpressionFixup unit tests

---
+++ Peach.Core.Test/Fixups/ExpressionFixupTests.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using Peach.Core.IO;
+using Peach.Core.Dom;
+using Peach.Core.Analyzers;
+
+namespace Peach.Core.Test.Fixups
+{
+    [TestFixture]
+    class ExpressionFixupTests
+    {
+       
+
+        [Test]
+        public void IntTest()
+        {
+            
+
+            // standard test
+
+            string xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n" +
+                "<Peach>" +
+                "   <DataModel name=\"TheDataModel\">" +
+                "       <Number name=\"CRC\" size=\"32\" signed=\"false\">" +
+                "           <Fixup class=\"ExpressionFixup\">" +
+                "               <Param name=\"ref\" value=\"Data\"/>" +
+                "               <Param name=\"expression\" value=\"42\"/>" +
+                "           </Fixup>" +
+                "       </Number>" +
+                "       <Blob name=\"Data\" value=\"12345\"/>" +
+                "   </DataModel>" +
+
+                "   <StateModel name=\"TheState\" initialState=\"Initial\">" +
+                "       <State name=\"Initial\">" +
+                "           <Action type=\"output\">" +
+                "               <DataModel ref=\"TheDataModel\"/>" +
+                "           </Action>" +
+                "       </State>" +
+                "   </StateModel>" +
+
+                "   <Test name=\"TheTest\">" +
+                "       <StateModel ref=\"TheState\"/>" +
+                "       <Publisher class=\"Stdout\"/>" +
+                "   </Test>" +
+
+                "   <Run name=\"DefaultRun\">" +
+                "       <Test ref=\"TheTest\"/>" +
+                "   </Run>" +
+                "</Peach>";
+
+            PitParser parser = new PitParser();
+
+            Dom.Dom dom = parser.asParser(new Dictionary<string, string>(), new MemoryStream(ASCIIEncoding.ASCII.GetBytes(xml)));
+
+            RunConfiguration config = new RunConfiguration();
+            config.singleIteration = true;
+            
+            byte[] testValue = null;
+            Dom.Action.Finished += delegate(Dom.Action action) { testValue = action.dataModel[0].Value.Value; };
+
+            Engine e = new Engine(null);
+            e.config = config;
+            e.startFuzzing(dom, config);
+
+            // verify values
+            
+            Assert.AreEqual(testValue, new byte[] { 42, 0x00, 0x00, 0x00 });
+
+            
+        }
+
+        [Test]
+        public void StringTest()
+        {
+
+
+            // standard test
+
+            string xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n" +
+                "<Peach>" +
+                "   <DataModel name=\"TheDataModel\">" +
+                "       <Blob name=\"CRC\" length='4' signed=\"false\">" +
+                "           <Fixup class=\"ExpressionFixup\">" +
+                "               <Param name=\"ref\" value=\"Data\"/>" +
+                "               <Param name=\"expression\" value=\"'AABB'\"/>" +
+                "           </Fixup>" +
+                "       </Blob>" +
+                "       <Blob name=\"Data\" value=\"12345\"/>" +
+                "   </DataModel>" +
+
+                "   <StateModel name=\"TheState\" initialState=\"Initial\">" +
+                "       <State name=\"Initial\">" +
+                "           <Action type=\"output\">" +
+                "               <DataModel ref=\"TheDataModel\"/>" +
+                "           </Action>" +
+                "       </State>" +
+                "   </StateModel>" +
+
+                "   <Test name=\"TheTest\">" +
+                "       <StateModel ref=\"TheState\"/>" +
+                "       <Publisher class=\"Stdout\"/>" +
+                "   </Test>" +
+
+                "   <Run name=\"DefaultRun\">" +
+                "       <Test ref=\"TheTest\"/>" +
+                "   </Run>" +
+                "</Peach>";
+
+            PitParser parser = new PitParser();
+
+            Dom.Dom dom = parser.asParser(new Dictionary<string, string>(), new MemoryStream(ASCIIEncoding.ASCII.GetBytes(xml)));
+
+            RunConfiguration config = new RunConfiguration();
+            config.singleIteration = true;
+
+            byte[] testValue = null;
+            Dom.Action.Finished += delegate(Dom.Action action) { testValue = action.dataModel[0].Value.Value; };
+
+            Engine e = new Engine(null);
+            e.config = config;
+            e.startFuzzing(dom, config);
+
+            // verify values
+
+            Assert.AreEqual(new byte[] { 0x41, 0x41, 0x42, 0x42 }, testValue);
+
+
+        }
+
+
+        [Test]
+        public void ByteTest()
+        {
+
+
+            // standard test
+
+            string xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n" +
+                "<Peach>" +
+                "   <DataModel name=\"TheDataModel\">" +
+                "       <Blob name=\"CRC\" length=\"4\" signed=\"false\">" +
+                "           <Fixup class=\"ExpressionFixup\">" +
+                "               <Param name=\"ref\" value=\"Data\"/>" +
+                "               <Param name=\"expression\" value=\"'\\x00\\x01\\xff\\x00'\"/>" +
+                "           </Fixup>" +
+                "       </Blob>" +
+                "       <Blob name=\"Data\" value=\"12345\"/>" +
+                "   </DataModel>" +
+
+                "   <StateModel name=\"TheState\" initialState=\"Initial\">" +
+                "       <State name=\"Initial\">" +
+                "           <Action type=\"output\">" +
+                "               <DataModel ref=\"TheDataModel\"/>" +
+                "           </Action>" +
+                "       </State>" +
+                "   </StateModel>" +
+
+                "   <Test name=\"TheTest\">" +
+                "       <StateModel ref=\"TheState\"/>" +
+                "       <Publisher class=\"Stdout\"/>" +
+                "   </Test>" +
+
+                "   <Run name=\"DefaultRun\">" +
+                "       <Test ref=\"TheTest\"/>" +
+                "   </Run>" +
+                "</Peach>";
+
+            PitParser parser = new PitParser();
+
+            Dom.Dom dom = parser.asParser(new Dictionary<string, string>(), new MemoryStream(ASCIIEncoding.ASCII.GetBytes(xml)));
+
+            RunConfiguration config = new RunConfiguration();
+            config.singleIteration = true;
+
+            byte[] testValue = null;
+            Dom.Action.Finished += delegate(Dom.Action action) { testValue = action.dataModel[0].Value.Value; };
+
+            Engine e = new Engine(null);
+            e.config = config;
+            e.startFuzzing(dom, config);
+
+            // verify values
+
+            Assert.AreEqual(new byte[] { 0x00, 0x01, 0xff, 0x00 },testValue);
+
+
+        }
+
+    }
+}
+
+// end
---
+++ Peach.Core/Fixups/ExpressionFixup.cs
@@ -0,0 +1,103 @@
+﻿
+//
+// Copyright (c) Michael Eddington
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights 
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+// copies of the Software, and to permit persons to whom the Software is 
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in	
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+// Authors:
+//   Mikhail Davidov (sirus@haxsys.net)
+
+// $Id$
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Peach.Core.Dom;
+
+
+namespace Peach.Core.Fixups
+{
+    [FixupAttribute("ExpressionFixup", "XOR bytes of data.")]
+    //checksums.ExpressionFixup is for backwards compatibility with Peach2
+    [FixupAttribute("checksums.ExpressionFixup", "XOR bytes of data.")]
+    [ParameterAttribute("ref", typeof(DataElement), "Reference to data element", true)]
+    [ParameterAttribute("expression", typeof(string), "Expression returning string or int", true)]
+    [Serializable]
+    public class ExpressionFixup : Fixup
+    {
+        public ExpressionFixup(Dictionary<string, Variant> args)
+            : base(args)
+        {
+            if (!args.ContainsKey("ref"))
+                throw new PeachException("Error, ExpressionFixup requires a 'ref' argument!");
+
+            if (!args.ContainsKey("expression"))
+                throw new PeachException("Error, ExpressionFixup requires an 'expression' argument!");
+        }
+
+        protected override Variant fixupImpl(DataElement obj)
+        {
+            string objRef = (string)args["ref"];
+            string expression = (string)args["expression"];
+
+            DataElement from = obj.find(objRef);
+
+            if (from == null)
+                throw new PeachException(string.Format("ExpressionFixup could not find ref element '{0}'", objRef));
+
+            byte[] data = from.Value.Value;
+
+            Dictionary<string, object> state = new Dictionary<string, object>();
+            state["self"] = this;
+            state["ref"] = from;
+            state["data"] = data;
+            try
+            {
+                object value = Scripting.EvalExpression(expression, state);
+                
+                if (value is string)
+                {
+                    string str = value as string;
+                    byte[] strbytes = new byte[str.Length];
+                    
+                    for(int i=0; i<strbytes.Length; ++i)
+                        strbytes[i] = (byte)str[i];
+
+                    return new Variant(strbytes);
+                }
+                else if (value is int)
+                    return new Variant(Convert.ToInt32(value));
+                else
+                {
+                    throw new PeachException(
+                        string.Format("ExpressionFixup expected a return value of string or int but got '{0}'", value.GetType().Name));
+                }
+
+            }
+            catch (System.Exception ex)
+            {
+                throw new PeachException(
+                    string.Format("ExpressionFixup expression threw an exception!\nExpression: {0}\n Exception: {1}", expression, ex.ToString()));
+            }   
+        }
+    }
+}
+
+// end
