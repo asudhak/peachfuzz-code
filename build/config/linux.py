e9901ae301f554231163f710e3c84320780ace5c
shinze <shinze@9c48eac0-7620-0410-89eb-927fe2045f54>
2012-07-16 01:17:23 +0000

    Getting the workd to build on linux via waf.

--- build/config/linux.py
+++ build/config/linux.py
@@ -2,20 +2,67 @@ from waflib import Utils
 
 archs = [ 'x86', 'x86_64' ]
 tools = [
-	'mcs',
+	'gcc',
+	'gxx',
+	'cs',
 	'resx',
+	'csprogram',
 ]
 
 def prepare(conf):
-	# set up paths in conf.env that will be used by the tools
-	pass
+	env = conf.env
+
+	env['TOOLCHAIN_PATH'] = None
+	env['TOOLCHAIN_LIBS'] = None
+	env['TOOLCHAIN_INCS'] = None
+
+	env['TOOLCHAIN_MCS']  = 'dmcs'
 
 def configure(conf):
-	# setup CFLAGS, LINKFLAGS, etc
+	env = conf.env
+
+	env.append_value('CSFLAGS', [
+		'/warn:4',
+		'/define:PEACH',
+	])
+
+	env['CSPLATFORM'] = 'anycpu'
+
+	arch_flags = [
+		'-m%s' % ('64' in env.SUBARCH and '64' or '32'),
+	]
+
+	cflags = [
+		'-pipe',
+		'-Werror',
+		'-Wno-unused',
+	]
+	
+	env.append_value('CFLAGS', arch_flags + cflags)
+	env.append_value('CXXFLAGS', arch_flags + cflags)
+	env.append_value('LINKFLAGS', arch_flags)
+	
 	return [ 'debug', 'release' ]
 
 def debug(env):
-	pass
+	env.CSDEBUG = 'full'
+
+	cflags = [
+		'-ggdb',
+	]
+
+	env.append_value('CSFLAGS', ['/define:DEBUG,TRACE'])
+	env.append_value('DEFINES', ['DEBUG'])
+	env.append_value('CFLAGS', cflags)
+	env.append_value('CXXFLAGS', cflags)
 
 def release(env):
-	pass
+	env.CSDEBUG = 'pdbonly'
+
+	cflags = [
+		'-O3',
+	]
+
+	env.append_value('CSFLAGS', ['/define:TRACE', '/optimize+'])
+	env.append_value('CFLAGS', cflags)
+	env.append_value('CXXFLAGS', cflags)
