216ffd6c85cdf8c9ae3470e2a887ee049d4c56c8
meddingt <meddingt@9c48eac0-7620-0410-89eb-927fe2045f54>
2012-07-19 18:26:17 +0000

    Updated the Peach XSD namespace to 2012.

--- ComTest/ComTest_i.h
+++ ComTest/ComTest_i.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Fri Jul 13 22:10:37 2012
+/* at Mon Jul 16 22:01:11 2012
  */
 /* Compiler settings for ComTest.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
