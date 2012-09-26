4c23f80a7c5aedb43b917487334bd301cf8ae9a0
Thomas Nagy <tnagy2pow10@gmail.com>
2012-09-26 18:43:10 +0200

    waf-1.7.4

--- waflib/Context.py
+++ waflib/Context.py
@@ -11,10 +11,10 @@ from waflib import Utils, Errors, Logs
 import waflib.Node
 
 # the following 3 constants are updated on each new release (do not touch)
-HEXVERSION=0x1070300
+HEXVERSION=0x1070400
 """Constant updated on new releases"""
 
-WAFVERSION="1.7.3"
+WAFVERSION="1.7.4"
 """Constant updated on new releases"""
 
 WAFREVISION="a7e69d6b81b04729804754c4d5214da063779a65"
