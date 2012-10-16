99a9815c2509d5a79a1daeaf836e76a1a4fea9b1
Seth Hinze <seth@dejavusecurity.com>
2012-10-16 16:33:10 -0700

    Enhancing test to demonstrate padding failure.

--- Peach.Core.Test/CrackingTests/PaddingTests.cs
+++ Peach.Core.Test/CrackingTests/PaddingTests.cs
@@ -44,16 +44,16 @@ namespace Peach.Core.Test.CrackingTests
 	[TestFixture]
 	public class PaddingTests
 	{
+		static string xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n<Peach>\n" +
+			"	<DataModel name=\"TheDataModel\">" +
+			"		<Blob length=\"1\" valueType=\"hex\" value=\"00\" />" +
+			"		<Padding aligned=\"true\" alignment=\"16\" /> " +
+			"	</DataModel>" +
+			"</Peach>";
+
 		[Test]
 		public void CrackPadding1()
 		{
-			string xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n<Peach>\n" +
-				"	<DataModel name=\"TheDataModel\">" +
-				"		<Blob length=\"1\" />" +
-				"		<Padding aligned=\"true\" alignment=\"16\" /> "+
-				"	</DataModel>" +
-				"</Peach>";
-
 			PitParser parser = new PitParser();
 			Dom.Dom dom = parser.asParser(null, new MemoryStream(ASCIIEncoding.ASCII.GetBytes(xml)));
 
@@ -66,6 +66,28 @@ namespace Peach.Core.Test.CrackingTests
 
 			Assert.AreEqual(new byte[] { 1 }, (byte[])dom.dataModels[0][0].DefaultValue);
 			Assert.AreEqual(8, ((BitStream)dom.dataModels[0][1].DefaultValue).LengthBits);
+
+			var value = dom.dataModels[0].Value;
+			Assert.AreEqual(2, value.LengthBytes);
+			Assert.AreEqual(1, value.ReadByte());
+			Assert.AreEqual(0, value.ReadByte());
+
+		}
+
+		[Test]
+		public void GeneratePadding()
+		{
+			PitParser parser = new PitParser();
+			Dom.Dom dom = parser.asParser(null, new MemoryStream(ASCIIEncoding.ASCII.GetBytes(xml)));
+
+			var data = dom.dataModels[0].Value;
+			Assert.AreEqual(16, data.LengthBits);
+
+			var blob = dom.dataModels[0][0];
+			Assert.AreEqual(8, blob.Value.LengthBits);
+
+			var padding = dom.dataModels[0][1];
+			Assert.AreEqual(8, padding.Value.LengthBits);
 		}
 	}
 }
