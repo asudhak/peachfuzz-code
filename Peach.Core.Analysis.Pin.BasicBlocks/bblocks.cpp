b9169f883defb788862a1aff50c65b9b32ef6263
Seth Hinze <seth@dejavusecurity.com>
2012-10-24 15:02:18 -0700

    64bit build fixes.

--- Peach.Core.Analysis.Pin.BasicBlocks/bblocks.cpp
+++ Peach.Core.Analysis.Pin.BasicBlocks/bblocks.cpp
@@ -75,12 +75,12 @@ VOID PIN_FAST_ANALYSIS_CALL rememberBlock(ADDRINT bbl)
 	ret = setKnownBlocks.insert(bbl);
 	if(ret.second == true)
 	{
-		fprintf(trace, "%u\n", (size_t)bbl);
+		fprintf(trace, "%zu\n", (size_t)bbl);
 		fflush(trace);
 
 		if(haveExisting)
 		{
-			fprintf(existing, "%u\n", (size_t)bbl);
+			fprintf(existing, "%zu\n", (size_t)bbl);
 			fflush(existing);
 		}
 	}
@@ -120,7 +120,7 @@ int main(int argc, char * argv[])
 		haveExisting = TRUE;
 		while(!feof(existing))
 		{
-			if(fscanf(existing, "%u\n", &block) < 4)
+			if(fscanf(existing, "%zu\n", &block) < 4)
 				setExistingBlocks.insert(block);
 		}
 
