c99ed30b2064f88a74aefee49062ddb9671c08d9
Seth Hinze <seth@dejavusecurity.com>
2012-12-06 15:39:41 -0800

    Adding tests for ProcessInfo.

---
+++ Peach.Core.Test.OS.OSX/TestBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO;
+using NLog;
+using NLog.Targets;
+using NLog.Config;
+using NUnit.Framework;
+
+namespace Peach.Core.Test.OS.OSX
+{
+	[SetUpFixture]
+	class TestBase
+	{
+		[SetUp]
+		public void Initialize()
+		{
+			ColoredConsoleTarget consoleTarget = new ColoredConsoleTarget();
+			consoleTarget.Layout = "${date:format=HH\\:MM\\:ss} ${logger} ${message}";
+
+			LoggingConfiguration config = new LoggingConfiguration();
+			config.AddTarget("console", consoleTarget);
+
+			LoggingRule rule = new LoggingRule("*", LogLevel.Debug, consoleTarget);
+			config.LoggingRules.Add(rule);
+
+			LogManager.Configuration = config;
+		}
+	}
+}
