766ea093ec29b17155c4b5431b0cb5d7a9ec9fe9
Seth Hinze <seth@dejavusecurity.com>
2012-12-19 15:27:00 -0800

    Implementing process killer monitor. fixes: #196

---
+++ Peach.Core.Test/Monitors/ProcessKillerMoniorTests.cs
@@ -0,0 +1,109 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using Peach.Core.Analyzers;
+using Proc = System.Diagnostics.Process;
+using System.Diagnostics;
+
+namespace Peach.Core.Test.Monitors
+{
+	[TestFixture]
+	class ProcessKillerMonitorTests
+	{
+		string MakeXml(string folder)
+		{
+			string template = @"
+<Peach>
+	<DataModel name='TheDataModel'>
+		<String value='Hello' mutable='false'/>
+	</DataModel>
+
+	<StateModel name='TheState' initialState='Initial'>
+		<State name='Initial'>
+			<Action type='output'>
+				<DataModel ref='TheDataModel'/>
+			</Action>
+		</State>
+	</StateModel>
+
+	<Agent name='LocalAgent'>
+		<Monitor class='ProcessKiller'>
+			<Param name='ProcessNames' value='{0}'/>
+		</Monitor>
+	</Agent>
+
+	<Test name='Default'>
+		<Agent ref='LocalAgent'/>
+		<StateModel ref='TheState'/>
+		<Publisher class='Null'/>
+	</Test>
+</Peach>";
+
+			var ret = string.Format(template, folder);
+			return ret;
+		}
+
+		void Run(string proccessNames, Engine.IterationStartingEventHandler OnIterStart = null)
+		{
+			string xml = MakeXml(proccessNames);
+
+			PitParser parser = new PitParser();
+
+			Dom.Dom dom = parser.asParser(null, new MemoryStream(ASCIIEncoding.ASCII.GetBytes(xml)));
+			dom.tests[0].includedMutators = new List<string>();
+			dom.tests[0].includedMutators.Add("StringCaseMutator");
+
+			RunConfiguration config = new RunConfiguration();
+
+			Engine e = new Engine(null);
+			e.config = config;
+			if (OnIterStart != null)
+				e.IterationStarting += OnIterStart;
+			e.startFuzzing(dom, config);
+		}
+
+		[Test]
+		public void TestBadProcss()
+		{
+			// Specify a process name that is not running
+			Run("some_invalid_process");
+		}
+
+		[Test]
+		public void TestProcss()
+		{
+			// Specify a process name that is not running
+			Run(testProcess, IterationStarting);
+
+			var procs = Proc.GetProcessesByName(testProcess);
+			foreach (var p in procs)
+				p.Close();
+
+			Assert.True(madeProcess);
+			Assert.AreEqual(0, procs.Length);
+		}
+
+		string testProcess = GetTestProcess();
+		bool madeProcess = false;
+
+		void IterationStarting(RunContext context, uint currentIteration, uint? totalIterations)
+		{
+			Proc p = new Proc();
+			p.StartInfo = new ProcessStartInfo(testProcess);
+			madeProcess = p.Start();
+			p.Close();
+		}
+
+		static string GetTestProcess()
+		{
+			if (Platform.GetOS() == Platform.OS.Windows)
+				return "notepad";
+			else
+				return "tail";
+		}
+	}
+}
---
+++ Peach.Core/Agent/Monitors/ProcessKillerMonitor.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NLog;
+using Proc = System.Diagnostics.Process;
+
+namespace Peach.Core.Agent.Monitors
+{
+	[Monitor("ProcessKiller", true)]
+	[Parameter("ProcessNames", typeof(string[]), "Comma seperated list of process to kill.")]
+	public class ProcessKillerMonitor : Peach.Core.Agent.Monitor
+	{
+		public string[] ProcessNames { get; private set; }
+
+		public ProcessKillerMonitor(IAgent agent, string name, Dictionary<string, Variant> args)
+			: base(agent, name, args)
+		{
+			ParameterParser.Parse(this, args);
+		}
+
+		public override void StopMonitor()
+		{
+		}
+
+		public override void SessionStarting()
+		{
+		}
+
+		public override void SessionFinished()
+		{
+		}
+
+		public override void IterationStarting(uint iterationCount, bool isReproduction)
+		{
+		}
+
+		public override bool IterationFinished()
+		{
+			foreach (var item in ProcessNames)
+				Kill(item);
+
+			return false;
+		}
+
+		public override bool DetectedFault()
+		{
+			return false;
+		}
+
+		public override Fault GetMonitorData()
+		{
+			return null;
+		}
+
+		public override bool MustStop()
+		{
+			return false;
+		}
+
+		public override Variant Message(string name, Variant data)
+		{
+			return null;
+		}
+
+		static NLog.Logger logger = LogManager.GetCurrentClassLogger();
+
+		void Kill(string processName)
+		{
+			Proc[] procs = Proc.GetProcessesByName(processName);
+
+			foreach (var p in procs)
+			{
+				try
+				{
+					if (!p.HasExited)
+					{
+						p.Kill();
+						p.WaitForExit();
+					}
+				}
+				catch (Exception ex)
+				{
+					logger.Debug("Unable to kill process '{0}' (pid: {2}). {1}", processName, p.Id, ex.Message);
+				}
+				finally
+				{
+					p.Close();
+				}
+			}
+		}
+	}
+}
