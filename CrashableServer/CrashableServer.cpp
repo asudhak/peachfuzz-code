e8314e0520c6569369b24ebc157a8653256a7f4d
Seth Hinze <seth@dejavusecurity.com>
2013-02-01 10:25:25 -0800

    Fixing indenting.

--- CrashableServer/CrashableServer.cpp
+++ CrashableServer/CrashableServer.cpp
@@ -10,111 +10,112 @@ int Run(const char* host, int port);
 
 int main(int argc, char* argv[])
 {
-    WSAData wsaData;
-    int ret;
-    const char* host;
-    int port;
+	WSAData wsaData;
+	int ret;
+	const char* host;
+	int port;
+	int retval = 0;
 
-    // Do we have enough command line arguments?
-    if (argc < 2) {
-	fprintf(stderr, "usage: %s <server-address> [server-port]\n", argv[0]);
-	fprintf(stderr, "\tIf you don't pass server-port, it defaults to %d.\n", kDefaultServerPort);
-        return 1;
-    }
+	// Do we have enough command line arguments?
+	if (argc < 2) {
+		fprintf(stderr, "usage: %s <server-address> [server-port]\n", argv[0]);
+		fprintf(stderr, "\tIf you don't pass server-port, it defaults to %d.\n", kDefaultServerPort);
+		return 1;
+	}
 
-    host = argv[1];
-    port = (argc >= 3) ? atoi(argv[2]) : kDefaultServerPort;
+	host = argv[1];
+	port = (argc >= 3) ? atoi(argv[2]) : kDefaultServerPort;
 
-    if (argc > 3) {
-	fprintf(stderr, "%d extra argument%s ignored.  FYI.\n", argc - 3, argc == 4 ? "" : "s");
-    }
+	if (argc > 3) {
+		fprintf(stderr, "%d extra argument%s ignored.  FYI.\n", argc - 3, argc == 4 ? "" : "s");
+	}
 
-    if ((ret = WSAStartup(MAKEWORD(1, 1), &wsaData)) != 0) {
-	fprintf(stderr, "WSAStartup() returned error code %d.\n", ret);
-        return 255;
-    }
+	if ((ret = WSAStartup(MAKEWORD(1, 1), &wsaData)) != 0) {
+		fprintf(stderr, "WSAStartup() returned error code %d.\n", ret);
+		return 255;
+	}
 
-    int retval = Run(host, port);
+	int retval = Run(host, port);
 
-    WSACleanup();
-    return retval;
+	WSACleanup();
+	return retval;
 }
 
 int Run(const char* host, int port)
 {
-    SOCKET listener;
+	SOCKET listener;
 
-    printf("Establishing the listener...\n");
+	printf("Establishing the listener...\n");
 
-    listener = SetUpListener(host, htons((u_short)port));
-    if (listener == INVALID_SOCKET) {
-	fprintf(stderr, "\nestablish listener error: %d\n", WSAGetLastError());
-        return 3;
-    }
-
-    for (;;) {
-	SOCKET socket;
-        sockaddr_in remote;
-
-	printf("Waiting for a connection...\n");
-
-        socket = AcceptConnection(listener, &remote);
-        if (socket == INVALID_SOCKET) {
-	    fprintf(stderr, "\naccept connection error: %d\n", WSAGetLastError());
-	    return 3;
+	listener = SetUpListener(host, htons((u_short)port));
+	if (listener == INVALID_SOCKET) {
+		fprintf(stderr, "\nestablish listener error: %d\n", WSAGetLastError());
+		return 3;
 	}
 
-	printf("Accepted connection from %s:%d.\n",
-	       inet_ntoa(remote.sin_addr),
-	       ntohs(remote.sin_port));
+	for (;;) {
+		SOCKET socket;
+		sockaddr_in remote;
 
-        if (!EchoIncomingPackets(socket)) {
-	    fprintf(stderr, "\necho incoming packets error: %d\n", WSAGetLastError());
-	    return 3;
+		printf("Waiting for a connection...\n");
+
+		socket = AcceptConnection(listener, &remote);
+		if (socket == INVALID_SOCKET) {
+			fprintf(stderr, "\naccept connection error: %d\n", WSAGetLastError());
+			return 3;
+		}
+
+		printf("Accepted connection from %s:%d.\n",
+			inet_ntoa(remote.sin_addr),
+			ntohs(remote.sin_port));
+
+		if (!EchoIncomingPackets(socket)) {
+			fprintf(stderr, "\necho incoming packets error: %d\n", WSAGetLastError());
+			return 3;
+		}
+
+		printf("Shutting connection down...\n");
+
+		if (closesocket(socket) != 0) {
+			fprintf(stderr, "\nshutdown connection error: %d\n", WSAGetLastError());
+			return 3;
+		}
+
+		printf("Connection is down.\n");
 	}
-
-	printf("Shutting connection down...\n");
-
-        if (closesocket(socket) != 0) {
-	    fprintf(stderr, "\nshutdown connection error: %d\n", WSAGetLastError());
-	    return 3;
-	}
-
-	printf("Connection is down.\n");
-    }
 }
 
 SOCKET SetUpListener(const char* host, int port)
 {
-    SOCKET listener;
-    u_long addr;
-    sockaddr_in sa;
-    
-    addr = inet_addr(host);
-    if (addr == INADDR_NONE)
-	return INVALID_SOCKET;
+	SOCKET listener;
+	u_long addr;
+	sockaddr_in sa;
 
-    listener = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (listener == INVALID_SOCKET)
-	return INVALID_SOCKET;
+	addr = inet_addr(host);
+	if (addr == INADDR_NONE)
+		return INVALID_SOCKET;
 
-    sa.sin_family = AF_INET;
-    sa.sin_addr.s_addr = addr;
-    sa.sin_port = (u_short)port;
+	listener = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (listener == INVALID_SOCKET)
+		return INVALID_SOCKET;
 
-    if (bind(listener, (sockaddr*)&sa, sizeof(sa)) == SOCKET_ERROR)
-	return INVALID_SOCKET;
+	sa.sin_family = AF_INET;
+	sa.sin_addr.s_addr = addr;
+	sa.sin_port = (u_short)port;
 
-    if (listen(listener, 1) == SOCKET_ERROR)
-	return INVALID_SOCKET;
+	if (bind(listener, (sockaddr*)&sa, sizeof(sa)) == SOCKET_ERROR)
+		return INVALID_SOCKET;
 
-    return listener;
+	if (listen(listener, 1) == SOCKET_ERROR)
+		return INVALID_SOCKET;
+
+	return listener;
 }
 
 SOCKET AcceptConnection(SOCKET listener, sockaddr_in* remote)
 {
-    socklen_t size = sizeof(*remote);
-    return accept(listener, (sockaddr*)remote, &size);
+	socklen_t size = sizeof(*remote);
+	return accept(listener, (sockaddr*)remote, &size);
 }
 
 void CrashMe(char* in)
@@ -146,26 +147,25 @@ void CrashMe(char* in)
 
 bool EchoIncomingPackets(SOCKET socket)
 {
-    char* buf = (char*)malloc(kBufferSize);
-    int len;
+	char* buf = (char*)malloc(kBufferSize);
+	int len;
 
-    do {
-        len = recv(socket, buf, kBufferSize, 0);
-        if (len > 0) {
-            printf("Received %d bytes from client.\n", len);
+	do {
+		len = recv(socket, buf, kBufferSize, 0);
+		if (len > 0) {
+			printf("Received %d bytes from client.\n", len);
 
+			// Add a silly stack overflow
+			if (len >= 1024) {
+				CrashMe(buf);
+			}
+		}
+		else if (len == SOCKET_ERROR) {
+			return false;
+		}
+	} while (len != 0);
 
-	    // Add a silly stack overflow
-	    if (len >= 1024) {
-		CrashMe(buf);
-	    }
-        }
-        else if (len == SOCKET_ERROR) {
-            return false;
-        }
-    } while (len != 0);
+	printf("Connection closed by peer.\n");
 
-    printf("Connection closed by peer.\n");
-
-    return true;
+	return true;
 }
