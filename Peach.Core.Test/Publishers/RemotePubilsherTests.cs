20d4b7cd306ed6164512c92a04d7530873a6d457
Seth Hinze <seth@dejavusecurity.com>
2013-02-06 12:31:04 -0800

    More remote publisher tests.

--- Peach.Core.Test/Publishers/RemotePubilsherTests.cs
+++ Peach.Core.Test/Publishers/RemotePubilsherTests.cs
@@ -41,6 +41,70 @@ namespace Peach.Core.Test.Publishers
 	</Test>
 </Peach>";
 
+		string raw_eth = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<Peach>
+	<DataModel name=""TheDataModel"">
+		<String value=""Hello!Hello!001122334455""/>
+	</DataModel>
+
+	<StateModel name=""TheStateModel"" initialState=""InitialState"">
+		<State name=""InitialState"">
+			<Action name=""Action1"" type=""output"">
+				<DataModel ref=""TheDataModel""/>
+			</Action>
+			<Action name=""Action2"" type=""input"">
+				<DataModel ref=""TheDataModel""/>
+			</Action>
+		</State>
+	</StateModel>
+
+	<Agent name=""LocalAgent"" location=""tcp://10.0.1.45:9001"">
+	</Agent>
+
+	<Test name=""Default"">
+		<Agent ref=""LocalAgent""/>
+		<StateModel ref=""TheStateModel""/>
+		<Publisher class=""Remote"">
+			<Param name=""Agent"" value=""LocalAgent""/>
+			<Param name=""Class"" value=""RawEther""/>
+			<Param name=""Interface"" value=""eth0""/>
+		</Publisher>
+	</Test>
+</Peach>";
+
+		string udp = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<Peach>
+	<DataModel name=""TheDataModel"">
+		<String value=""Hello!Hello!001122334455""/>
+	</DataModel>
+
+	<StateModel name=""TheStateModel"" initialState=""InitialState"">
+		<State name=""InitialState"">
+			<Action name=""Action1"" type=""output"">
+				<DataModel ref=""TheDataModel""/>
+			</Action>
+			<Action name=""Action2"" type=""input"">
+				<DataModel ref=""TheDataModel""/>
+			</Action>
+		</State>
+	</StateModel>
+
+	<Agent name=""LocalAgent"" location=""tcp://10.0.1.45:9001"">
+	</Agent>
+
+	<Test name=""Default"">
+		<Agent ref=""LocalAgent""/>
+		<StateModel ref=""TheStateModel""/>
+		<Publisher class=""Remote"">
+			<Param name=""Agent"" value=""LocalAgent""/>
+			<Param name=""Class"" value=""Udp""/>
+			<Param name=""Host"" value=""127.0.0.1""/>
+			<Param name=""SrcPort"" value=""12344""/>
+			<Param name=""Port"" value=""12345""/>
+		</Publisher>
+	</Test>
+</Peach>";
+
 		[Test]
 		public void TestCreate()
 		{
@@ -70,6 +134,30 @@ namespace Peach.Core.Test.Publishers
 
 			Assert.AreEqual(1, output.Length);
 			Assert.AreEqual("Hello", output[0]);
+		}
+
+		public void RunRemote(string xml)
+		{
+			PitParser parser = new PitParser();
+			Dom.Dom dom = parser.asParser(null, new MemoryStream(ASCIIEncoding.ASCII.GetBytes(xml)));
+
+			RunConfiguration config = new RunConfiguration();
+			config.singleIteration = true;
+
+			Engine e = new Engine(null);
+			e.startFuzzing(dom, config);
+		}
+
+		[Test, Ignore]
+		public void TestRaw()
+		{
+			RunRemote(raw_eth);
+		}
+
+		[Test, Ignore]
+		public void TestUdp()
+		{
+			RunRemote(udp);
 		}
 	}
 }
