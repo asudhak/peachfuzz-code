a5701ce9372efd42bd5d7440396011cd3dcfdd1c
Seth Hinze <seth@dejavusecurity.com>
2013-02-08 18:02:48 -0800

    Making tests deal with faulting on a control iteration.

--- Peach.Core.Test.OS.Windows/Agent/Monitors/PopupWatcherTest.cs
+++ Peach.Core.Test.OS.Windows/Agent/Monitors/PopupWatcherTest.cs
@@ -74,7 +74,7 @@ namespace Peach.Core.Test.Agent.Monitors
 			return ret;
 		}
 
-		void Run(Params parameters)
+		void Run(Params parameters, bool shouldFault)
 		{
 			string xml = MakeXml(parameters);
 
@@ -90,13 +90,28 @@ namespace Peach.Core.Test.Agent.Monitors
 
 			Engine e = new Engine(null);
 			e.Fault += _Fault;
-			e.startFuzzing(dom, config);
+
+			if (!shouldFault)
+			{
+				e.startFuzzing(dom, config);
+				return;
+			}
+
+			try
+			{
+				e.startFuzzing(dom, config);
+				Assert.Fail("Should throw.");
+			}
+			catch (PeachException ex)
+			{
+				Assert.AreEqual("Fault detected on control iteration.", ex.Message);
+			}
 		}
 
 		[Test, ExpectedException(ExpectedException = typeof(PeachException), ExpectedMessage = "Could not start monitor \"PopupWatcher\".  Monitor 'PopupWatcher' is missing required parameter 'WindowNames'.")]
 		public void TestNoWindow()
 		{
-			Run(new Params());
+			Run(new Params(), false);
 			Assert.Null(faults);
 		}
 
@@ -135,7 +150,7 @@ namespace Peach.Core.Test.Agent.Monitors
 
 			try
 			{
-				Run(new Params { { "WindowNames", windowName } });
+				Run(new Params { { "WindowNames", windowName } }, true);
 			}
 			finally
 			{
@@ -163,7 +178,7 @@ namespace Peach.Core.Test.Agent.Monitors
 
 			try
 			{
-				Run(new Params { { "WindowNames", "Window1,Window2," + windowName } });
+				Run(new Params { { "WindowNames", "Window1,Window2," + windowName } }, true);
 			}
 			finally
 			{
@@ -190,7 +205,7 @@ namespace Peach.Core.Test.Agent.Monitors
 
 			try
 			{
-				Run(new Params { { "WindowNames", windowName }, { "Fault", "true" } });
+				Run(new Params { { "WindowNames", windowName }, { "Fault", "true" } }, true);
 			}
 			finally
 			{
