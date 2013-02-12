f5c2adb44778665d164a2a8227966f2e2e84800a
Seth Hinze <seth@dejavusecurity.com>
2013-02-11 17:49:56 -0800

    Test we can create processes after getting an OutOfMemory exception.

--- Peach.Core.Test.OS.Linux/PlatformTests.cs
+++ Peach.Core.Test.OS.Linux/PlatformTests.cs
@@ -42,10 +42,82 @@ namespace Peach.Core.Test.OS.Linux
 				p.StartInfo = si;
 				p.Start();
 				p.WaitForExit();
+				Assert.True(p.HasExited);
+				p.Close();
 
 				Assert.Throws<ArgumentException>(delegate() { ProcessInfo.Instance.Snapshot(p); });
 			}
 		}
+
+		[Test]
+		public void TestProcess()
+		{
+			int i = 0;
+			try
+			{
+				for (; i < 10000; ++i)
+				{
+					if ((i % 500) == 0)
+						logger.Debug("Starting Process #{0}", i + 1);
+					using (Process p = new Process())
+					{
+						p.StartInfo.FileName = "/bin/echo";
+						p.StartInfo.Arguments = "-n \"\"";
+						p.StartInfo.UseShellExecute = false;
+						p.StartInfo.CreateNoWindow = true;
+						p.StartInfo.WorkingDirectory = "/";
+						p.Start();
+					}
+				}
+			}
+			finally
+			{
+				Assert.AreEqual(10000, i);
+			}
+		}
+
+		[Test]
+		public void TestOutOfMemory()
+		{
+			MemoryStream src = new MemoryStream();
+			MemoryStream dst = new MemoryStream();
+
+			byte[] b = new byte[1024*1024];
+			src.Write(b, 0, b.Length);
+
+			try
+			{
+				while (true)
+				{
+					src.Seek(0, SeekOrigin.Begin);
+					src.CopyTo(dst, (int)src.Length);
+				}
+			}
+			catch (OutOfMemoryException ex)
+			{
+				logger.Info("Out Of Memory: {0}", ex);
+			}
+
+			logger.Debug("Memory before: {0}", GC.GetTotalMemory(true));
+
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+			GC.Collect();
+
+			logger.Debug("Memory after: {0}", GC.GetTotalMemory(true));
+
+			src = null;
+			dst = null;
+
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+			GC.Collect();
+
+			logger.Debug("Memory final: {0}", GC.GetTotalMemory(true));
+
+			TestProcess();
+		}
+
 
 	}
 }
