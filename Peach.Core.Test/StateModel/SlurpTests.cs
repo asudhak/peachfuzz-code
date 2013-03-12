bf4d187f2b495ed93658451253872f46737396de
Seth Hinze <seth@dejavusecurity.com>
2013-03-12 16:22:36 -0700

    Verify slurp xpath can contain action name.

--- Peach.Core.Test/StateModel/SlurpTests.cs
+++ Peach.Core.Test/StateModel/SlurpTests.cs
@@ -73,5 +73,57 @@ namespace Peach.Core.Test.StateModel
 			Assert.AreEqual(state.actions[0].dataModel.Value.Value, state.actions[2].dataModel.Value.Value);
 			Assert.AreEqual(state.actions[0].dataModel.Value.Value, state.actions[3].dataModel.Value.Value);
 		}
+
+		[Test]
+		public void Test2()
+		{
+			string xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n" +
+				"<Peach>" +
+				"   <DataModel name=\"TheDataModel1\">" +
+				"       <String name=\"String1\" value=\"1234567890\"/>" +
+				"   </DataModel>" +
+				"   <DataModel name=\"TheDataModel2\">" +
+				"       <String name=\"String2\" value=\"Hello World!\"/>" +
+				"   </DataModel>" +
+
+				"   <StateModel name=\"TheStateModel\" initialState=\"InitialState\">" +
+				"       <State name=\"InitialState\">" +
+				"           <Action name=\"Action1\" type=\"output\">" +
+				"               <DataModel ref=\"TheDataModel1\"/>" +
+				"           </Action>" +
+
+				"           <Action name=\"Action2\" type=\"slurp\" valueXpath=\"//Action1/TheDataModel1/String1\" setXpath=\"//String2\" />" +
+
+				"           <Action name=\"Action3\" type=\"output\">" +
+				"               <DataModel ref=\"TheDataModel1\"/>" +
+				"           </Action>" +
+
+				"           <Action name=\"Action4\" type=\"output\">" +
+				"               <DataModel ref=\"TheDataModel2\"/>" +
+				"           </Action>" +
+				"       </State>" +
+				"   </StateModel>" +
+
+				"   <Test name=\"Default\">" +
+				"       <StateModel ref=\"TheStateModel\"/>" +
+				"       <Publisher class=\"Null\"/>" +
+				"   </Test>" +
+				"</Peach>";
+
+			PitParser parser = new PitParser();
+			Dom.Dom dom = parser.asParser(null, new MemoryStream(ASCIIEncoding.ASCII.GetBytes(xml)));
+
+			RunConfiguration config = new RunConfiguration();
+			config.singleIteration = true;
+
+			Engine e = new Engine(null);
+			e.startFuzzing(dom, config);
+
+			var stateModel = dom.tests[0].stateModel;
+			var state = stateModel.initialState;
+
+			Assert.AreEqual(state.actions[0].dataModel.Value.Value, state.actions[2].dataModel.Value.Value);
+			Assert.AreEqual(state.actions[0].dataModel.Value.Value, state.actions[3].dataModel.Value.Value);
+		}
 	}
 }
