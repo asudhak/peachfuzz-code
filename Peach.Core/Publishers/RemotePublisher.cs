04b69752dcbaa7cab5b5bdceeb0a55b39ccee626
Josh Dukes <josh@dejavusecurity.com>
2013-04-22 15:31:56 -0700

    added more debugging

--- Peach.Core/Publishers/RemotePublisher.cs
+++ Peach.Core/Publishers/RemotePublisher.cs
@@ -297,8 +297,9 @@ namespace Peach.Core.Publishers
 			{
 				PerformRemoting(delegate() { _publisher.setProperty(property, value); });
 			}
-			catch (System.Runtime.Remoting.RemotingException)
+			catch (System.Runtime.Remoting.RemotingException ex)
 			{
+				logger.Debug(ex);
 				try
 				{
 					RestartRemotePublisher();
