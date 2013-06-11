b0df27820f893b5e8c5ccb48135a48484d600031
Seth Hinze <seth@dejavusecurity.com>
2013-06-10 17:21:19 -0700

    Crack right from the file stream.

--- Peach.Core/MutationStrategies/RandomStrategy.cs
+++ Peach.Core/MutationStrategies/RandomStrategy.cs
@@ -235,13 +235,13 @@ namespace Peach.Core.MutationStrategies
 
 		private DataModel ApplyFileData(Dom.Action action, Data data)
 		{
-			byte[] fileBytes = null;
+			FileStream fileBytes = null;
 
 			for (int i = 0; i < 5 && fileBytes == null; ++i)
 			{
 				try
 				{
-					fileBytes = File.ReadAllBytes(data.FileName);
+					fileBytes = File.OpenRead(data.FileName);
 				}
 				catch (Exception ex)
 				{
