c152676ea7ee580107d463ab25e35f44fdced97a
Jordyn <jordyn@dejavusecurity.com>
2013-07-01 15:15:51 -0700

    Added RunCommand Monitor and Tests

---
+++ Peach.Core.Test/Monitors/RunCommandTests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Peach.Core;
+using Peach.Core.Dom;
+using Peach.Core.Analyzers;
+using Peach.Core.IO;
+using Peach.Core.Agent;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace Peach.Core.Test.Monitors
+{
+	[TestFixture]
+	class RunCommandTests
+	{
+		string MakeXml(string[] options)
+		{
+			string template = @"
+			<Peach>
+				<DataModel name='TheDataModel'>
+					<String value='Hello' mutable='false'/>
+				</DataModel>
+
+				<StateModel name='TheState' initialState='Initial'>
+					<State name='Initial'>
+						<Action type='output'>
+							<DataModel ref='TheDataModel'/>
+						</Action>
+					</State>
+				</StateModel>
+
+				<Agent name='LocalAgent'>
+					<Monitor class='RunCommand'>
+						<Param name='Command' value='{0}'/>
+						<Param name='Arguments' value='{1}'/>
+						<Param name='When' value='{2}'/>
+						<Param name='UseShell' value='true'/>
+					</Monitor>
+					{3}
+				</Agent>
+
+				<Test name='Default' replayEnabled='false'>
+					<Agent ref='LocalAgent'/>
+					<StateModel ref='TheState'/>
+					<Publisher class='Null'/>
+					<Strategy class='RandomDeterministic'/>
+				</Test>
+			</Peach>";
+
+			var ret = string.Format(template, options);
+			return ret;
+		}
+
+		void Run(string testName, string faultAgent = "")
+		{
+			string tempFile = Path.GetTempFileName();
+			string xml = MakeXml(new string[] { "echo", testName + ">" + tempFile, testName, faultAgent});
+
+			PitParser parser = new PitParser();
+			Dom.Dom dom = parser.asParser(null, new MemoryStream(ASCIIEncoding.ASCII.GetBytes(xml)));
+
+			RunConfiguration config = new RunConfiguration();
+			config.singleIteration = true;
+
+			Engine e = new Engine(null);
+			e.startFuzzing(dom, config);
+
+			string[] output = File.ReadAllLines(tempFile);
+
+			Assert.AreEqual(1, output.Length);
+			Assert.AreEqual(testName, output[0]);
+		}
+
+		//public enum When {OnStart, OnEnd, OnIterationStart, OnIterationEnd, OnFault};
+		[Test]
+		public void TestOnStart()
+		{
+			Run("OnStart");
+		}
+
+		[Test]
+		public void TestOnEnd()
+		{
+			Run("OnEnd");
+		}
+
+		[Test]
+		public void TestOnIterationStart()
+		{
+			Run("OnIterationStart");
+		}
+
+		[Test]
+		public void TestOnIterationEnd()
+		{
+			Run("OnIterationEnd");
+		}
+
+		[Test, ExpectedException(typeof(PeachException), ExpectedMessage = "Could not start monitor \"RunCommand\".  Monitor 'RunCommand' could not set value type parameter 'When' to 'null'.")]
+        public void TestNoWhen()
+		{
+			Run("");
+		}
+
+		[Test]
+		public void TestOnFault()
+		{
+			string faultAgent = @"
+			<Monitor class='FaultingMonitor'>
+				<Param name='Iteration' value='1'/>
+			</Monitor>";
+
+			try
+			{
+				Run("OnFault", faultAgent);
+				Assert.Fail("Should throw.");
+			}
+			catch (PeachException ex)
+			{
+				Assert.AreEqual("Fault detected on control iteration.", ex.Message);
+			}
+		}
+	}
+}
---
+++ Peach.Core/Agent/Monitors/RunCommand.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+
+using Peach.Core.Dom;
+
+using NLog;
+
+namespace Peach.Core.Agent.Monitors
+{
+	[Monitor("RunCommand", true)]
+	[Parameter("Command", typeof(string), "Command line command to run")]
+	[Parameter("Arguments", typeof(string), "Optional command line arguments", "")]
+	[Parameter("When", typeof(When), "Period _When the command should be ran")]
+	[Parameter("WaitForExitTimeout", typeof(int), "Wait for exit timeout value in milliseconds (-1 is infinite)", "10000")]
+	public class RunCommand  : Monitor
+	{
+		static NLog.Logger logger = LogManager.GetCurrentClassLogger();
+
+		public string Command { get; private set; }
+		public string Arguments { get; private set; }
+		public When _When { get; private set; }
+		public int WaitForExitTimeout { get; private set; }
+		public bool UseShell { get; private set; }
+
+		public enum When {OnStart, OnEnd, OnIterationStart, OnIterationEnd, OnFault};
+
+		public RunCommand(IAgent agent, string name, Dictionary<string, Variant> args)
+			: base(agent, name, args)
+		{
+			ParameterParser.Parse(this, args);
+		}
+
+		void _Start()
+		{
+			var startInfo = new ProcessStartInfo();
+			startInfo.FileName = createScript();
+			startInfo.UseShellExecute = UseShell;
+			//if (!string.IsNullOrEmpty(Arguments))
+			//		startInfo.Arguments = Arguments;
+
+			logger.Debug("_Start(): Starting process");
+
+			try
+			{
+				using (var p = new System.Diagnostics.Process())
+				{
+					p.StartInfo = startInfo;
+					p.Start();
+					p.WaitForExit();
+				}
+			}
+			catch (Exception ex)
+			{
+				throw new PeachException("Could not run command '" + Command + "'.  " + ex.Message + ".", ex);
+			}
+		}
+
+		private string createScript()
+		{
+			string extension = "";
+			if (System.Environment.OSVersion.Platform == PlatformID.Win32NT)
+				extension = "bat";
+			else
+				extension = "sh";
+
+			string path = Path.GetTempPath();
+			string fileName = Guid.NewGuid().ToString() + "." + extension;
+			string file = Path.Combine(path, fileName);
+			System.IO.File.WriteAllText(@file, Command + " " + Arguments);
+			
+			return file;
+		}
+
+		public override void IterationStarting(uint iterationCount, bool isReproduction)
+		{
+			if (_When == When.OnIterationStart)
+				_Start();
+		}
+
+		public override bool DetectedFault()
+		{
+			return false;
+		}
+
+		public override Fault GetMonitorData()
+		{
+			if (_When == When.OnFault)
+				_Start();
+
+			return null;
+		}
+
+		public override bool MustStop()
+		{
+			return false;
+		}
+
+		public override void StopMonitor()
+		{
+			return;
+		}
+
+		public override void SessionStarting()
+		{
+			if (_When == When.OnStart)
+				_Start();
+		}
+
+		public override void SessionFinished()
+		{
+			if (_When == When.OnEnd)
+				_Start();
+		}
+
+		public override bool IterationFinished()
+		{
+			if (_When == When.OnIterationEnd)
+				_Start();
+  		
+			return true;
+		}
+
+		public override Variant Message(string name, Variant data)
+		{
+			return null;
+		}
+	}
+}
