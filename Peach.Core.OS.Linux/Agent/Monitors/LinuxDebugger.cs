27936ea31f13fd4fcf9db23200b1d445000a4d8d
Seth Hinze <seth@dejavusecurity.com>
2013-07-23 19:04:18 -0700

    Fix hangs when using the monitor with stripped executables.

--- Peach.Core.OS.Linux/Agent/Monitors/LinuxDebugger.cs
+++ Peach.Core.OS.Linux/Agent/Monitors/LinuxDebugger.cs
@@ -50,6 +50,12 @@ handle SIGSEGV EXC_BAD_ACCESS EXC_BAD_INSTRUCTION EXC_ARITHMETIC stop print
 file {1}
 set args {2}
 
+# Stripped prorgrma don't have main, so run 'info target'
+# and look for 'Entry point: 0x0123456789' line
+# and run 'breakpoint *0x0123456789'
+
+python gdb.execute('break *%s' % (__import__('re').compile('Entry point:\s+(0x[0-9a-fA-F]+)').search(str(gdb.execute('info target', False, True))).groups()[0] or '0x0'), False, True)
+
 start
 python with open('{4}', 'w') as f: f.write(str(gdb.inferiors()[0].pid))
 cont
