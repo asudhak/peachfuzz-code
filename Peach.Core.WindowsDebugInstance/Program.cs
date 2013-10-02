09772c774736edc424db1fa0a541f33680b510e5
Seth Hinze <seth@dejavusecurity.com>
2013-10-02 14:05:47 -0700

    Turn on timebomb and allow disabling via cmd line argument.

--- Peach.Core.WindowsDebugInstance/Program.cs
+++ Peach.Core.WindowsDebugInstance/Program.cs
@@ -34,8 +34,11 @@ using System.Threading;
 using System.Runtime.Remoting;
 using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Channels.Ipc;
+using Peach.Core.Runtime;
 
 using NLog;
+using NLog.Config;
+using NLog.Targets;
 
 namespace Peach.Core.WindowsDebugInstance
 {
@@ -43,22 +46,83 @@ namespace Peach.Core.WindowsDebugInstance
 	{
 		static NLog.Logger logger = LogManager.GetCurrentClassLogger();
 
+		static bool debug = false;
+		static bool timebomb = true;
+		static string channelName = null;
+
+		static void Syntax()
+		{
+			Console.WriteLine("> Peach.Core.WindowsDebugInstance");
+			Console.WriteLine("> Copyright (c) Deja vu Security\n");
+
+			Console.WriteLine("Syntax:");
+			Console.WriteLine(" Peach.Core.WindowsDebugInstance.exe [--debug] IPC_CHANNEL_NAME\n\n");
+
+			throw new SyntaxException();
+		}
+
 		static void Main(string[] args)
 		{
-			if (args.Length != 1)
+			var DefaultForeground = Console.ForegroundColor;
+
+			try
 			{
-				Console.WriteLine("> Peach.Core.WindowsDebugInstance");
-				Console.WriteLine("> Copyright (c) Deja vu Security\n");
+				if (args.Length == 0)
+					Syntax();
 
-				Console.WriteLine("Syntax:");
-				Console.WriteLine(" Peach.Core.WindowsDebugInstance.exe IPC_CHANNEL_NAME\n\n");
+				var p = new OptionSet()
+				{
+					{ "h|?|help", v => Syntax() },
+					{ "debug", v => debug = true },
+					{ "timebomb=", v => timebomb = Convert.ToBoolean(v) },
+				};
 
-				return;
+				List<string> extra = p.Parse(args);
+
+				if (extra.Count != 1)
+					Syntax();
+
+				channelName = extra[0];
+
+				Run();
+			}
+			catch (SyntaxException)
+			{
+				// Ignore, thrown by syntax()
+			}
+			catch (OptionException oe)
+			{
+					Console.WriteLine(oe.Message +"\n"); 
+			}
+			finally
+			{
+				// HACK - Required on Mono with NLog 2.0
+				LogManager.Configuration = null;
+
+				// Reset console colors
+				Console.ForegroundColor = DefaultForeground;
+			}
+		}
+
+		static void Run()
+		{
+			// Enable debugging if asked for
+			if (debug)
+			{
+				var nconfig = new LoggingConfiguration();
+				var consoleTarget = new ColoredConsoleTarget();
+				nconfig.AddTarget("console", consoleTarget);
+				consoleTarget.Layout = "${logger} ${message}";
+
+				var rule = new LoggingRule("*", LogLevel.Debug, consoleTarget);
+				nconfig.LoggingRules.Add(rule);
+
+				LogManager.Configuration = nconfig;
 			}
 
-			logger.Debug("Starting up IPC listener: " + args[0]);
+			logger.Debug("Starting up IPC listener: {0}", channelName);
 
-			IpcChannel ipcChannel = new IpcChannel(args[0]);
+			IpcChannel ipcChannel = new IpcChannel(channelName);
 			ChannelServices.RegisterChannel(ipcChannel, false);
 			Peach.Core.Agent.Monitors.WindowsDebug.DebuggerInstance.LastHeartBeat = DateTime.Now;
 
@@ -79,11 +143,11 @@ namespace Peach.Core.WindowsDebugInstance
 					}
 
 					// Timebomb!
-					//if ((DateTime.Now - Peach.Core.Agent.Monitors.WindowsDebug.DebuggerInstance.LastHeartBeat).TotalSeconds > 30)
-					//{
-					//	logger.Debug("Last heartbeat over 30 seconds, exiting!");
-					//	return;
-					//}
+					if (timebomb && (DateTime.Now - Peach.Core.Agent.Monitors.WindowsDebug.DebuggerInstance.LastHeartBeat).TotalSeconds > 30)
+					{
+						logger.Debug("Last heartbeat over 30 seconds, exiting!");
+						return;
+					}
 				}
 			}
 			catch(Exception ex)
