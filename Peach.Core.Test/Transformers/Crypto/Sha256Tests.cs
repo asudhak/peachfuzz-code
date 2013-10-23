2acee391b44c39fe1057d2bcf92353c76aabca9f
Mick <mick@dejavusecurity.com>
2013-10-23 15:19:59 -0700

    added unit test

---
+++ Peach.Core.Test/Transformers/Crypto/Sha256Tests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using Peach.Core;
+using Peach.Core.Dom;
+using Peach.Core.Analyzers;
+
+namespace Peach.Core.Test.Transformers.Crypto
+{
+	[TestFixture]
+	class Sha256Tests : DataModelCollector
+	{
+		[Test]
+		public void Test1()
+		{
+			// standard test
+
+			string xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n" +
+				"<Peach>" +
+				"   <DataModel name=\"TheDataModel\">" +
+				"       <Block name=\"TheBlock\">" +
+				"           <Transformer class=\"Sha256\"/>" +
+				"           <Blob name=\"Data\" value=\"Hello\"/>" +
+				"       </Block>" +
+				"   </DataModel>" +
+
+				"   <StateModel name=\"TheState\" initialState=\"Initial\">" +
+				"       <State name=\"Initial\">" +
+				"           <Action type=\"output\">" +
+				"               <DataModel ref=\"TheDataModel\"/>" +
+				"           </Action>" +
+				"       </State>" +
+				"   </StateModel>" +
+
+				"   <Test name=\"Default\">" +
+				"       <StateModel ref=\"TheState\"/>" +
+				"       <Publisher class=\"Null\"/>" +
+				"   </Test>" +
+				"</Peach>";
+
+			PitParser parser = new PitParser();
+
+			Dom.Dom dom = parser.asParser(null, new MemoryStream(ASCIIEncoding.ASCII.GetBytes(xml)));
+
+			RunConfiguration config = new RunConfiguration();
+			config.singleIteration = true;
+
+			Engine e = new Engine(null);
+			e.startFuzzing(dom, config);
+
+			// verify values
+			// -- this is the pre-calculated result from Peach2.3 on the blob: "Hello"
+			byte[] precalcResult = new byte[] { 0x18, 0x5f, 0x8d, 0xb3, 0x22, 0x71, 0xfe, 0x25, 0xf5, 0x61, 0xa6, 0xfc, 0x93, 0x8b, 0x2e, 0x26, 0x43, 0x06, 0xec, 0x30, 0x4e, 0xda, 0x51, 0x80, 0x07, 0xd1, 0x76, 0x48, 0x26, 0x38, 0x19, 0x69 };
+			Assert.AreEqual(1, values.Count);
+			Assert.AreEqual(precalcResult, values[0].ToArray());
+		}
+	}
+}
+
+// end
